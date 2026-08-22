5ada1f93ef4a55f97c1d8774e50d02dc136c4eb1
M-Saad-Sohail <saadsohail232005@gmail.com>
2026-08-22 20:27:01 +0500

    Feat: optimized way to include sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,42 +3,19 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://www.opproductions.com';
 
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1,
-    },
-    {
-      url: `${baseUrl}/event-terms-and-conditions`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/first-light`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/our-team`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/production-and-media`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/contact`,
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 0.7,
-    },
+  const routes = [
+    '',
+    '/first-light',
+    '/our-team',
+    '/production-and-media',
+    '/about',
+    '/services',
+    '/contact',
   ];
+
+  return routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    changeFrequency: 'monthly',
+    priority: route === '' ? 1 : 0.8,
+  }));
 }
